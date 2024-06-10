edf70b118b53d2b4ec12b4cfdd9f131408e894a0
Fonojonzo <ian-guto@hotmail.com>
2024-06-10 18:28:19 -0300

    swagger fix

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerDocument from './swagger.json' with { type: "json" };
+import userRoutes from './server/src/routes/userRoutes.js'; 
+import peixeRoutes from './server/src/routes/peixeRoutes.js';
 import swaggerUi from 'swagger-ui-express';
-import { fileURLToPath } from 'url';
-import path from 'path';
+
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +20,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Conexão MongoDB
 mongoose.connect(process.env.MONGO_URI, {
@@ -34,20 +36,7 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Rota para o Swagger UI
-const swaggerDocument = require('./swagger.json');
-const CSS_URL = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist@4.1.0/swagger-ui.min.css';
-const options = {
-  customCssUrl: CSS_URL,
-};
-app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument, options), swaggerUi.setup(swaggerDocument, options));
-
-// Servir arquivos estáticos
-app.use(express.static('public'));
-
 // Rotas
-import userRoutes from './server/src/routes/userRoutes.js';
-import peixeRoutes from './server/src/routes/peixeRoutes.js';
 app.use('/api/users', userRoutes);
 app.use('/api/peixes', peixeRoutes);
 
