08201e64b99b7d2281ccdabb15a5b891cf50196f
Fonojonzo <ian-guto@hotmail.com>
2024-06-12 05:38:55 -0300

    swagger3

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -41,8 +41,8 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/peixes/:userId', async (req, res) => {
-  console.log('User ID LOG:', userId);
   try {
+    console.log('User ID LOG:', userId);
     const userId = req.params.userId;
 
     const peixesUsuario = await PeixesUsuario.find({ ID_usuario: userId });
