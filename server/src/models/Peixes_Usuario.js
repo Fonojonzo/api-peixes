31454f7aa591b467a9754cfe42a613f85abe5a22
Fonojonzo <ian-guto@hotmail.com>
2024-06-12 03:27:11 -0300

    update

--- server/src/models/Peixes_Usuario.js
+++ server/src/models/Peixes_Usuario.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
-const peixesUsuarioSchema = new mongoose.Schema({
-  ID_peixes: { type: Number, required: true },
+const peixeSchema = new mongoose.Schema({
   ID_usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  Nome: { type: String, required: true }
+  Especie: { type: String, required: true },
+  Nome: { type: String, required: true },
+  Tempo_alimentacao: { type: String, required: true },
+  Quantidade: { type: Number, required: true },
+  Alimentacao: { type: String, required: true },
+  Imagem: { type: String, required: true },
 });
 
-const PeixesUsuario = mongoose.model('PeixesUsuario', peixesUsuarioSchema, 'peixes_usuario');
+const Peixe = mongoose.model('Peixe', peixeSchema);
 
-export default PeixesUsuario;
+export default Peixe;
